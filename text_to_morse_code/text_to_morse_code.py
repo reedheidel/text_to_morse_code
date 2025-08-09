98ffb16197889c823a011bf28dd8b660b66ca998
reedheidel <154921468+reedheidel@users.noreply.github.com>
2025-08-09 19:28:01 +0200

    Update text_to_morse_code.py

--- text_to_morse_code/text_to_morse_code.py
+++ text_to_morse_code/text_to_morse_code.py
@@ -40,3 +40,14 @@ for i in range(length):
         output = output + " " + symbols.get(ask[i])
 
 print(output)   
+ask = input("type: ")
+
+
+length = len(ask)
+output = ""
+
+for i in range(length):
+    if ask[i] in symbols.keys():
+        output = output + " " + symbols.get(ask[i])
+
+print(output)   
